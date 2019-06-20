dc25c458ca28533d5ecba55db69e806281b4698c
wildcod <kanojias494@gmail.com>
2019-06-20 22:05:34 +0530

    signup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './component/common/header/header'
+import Signup from './container/signup/signup'
+import Header from './component/common/header/header';
 
 function App() {
   return (
     <div className="App">
        <Header />
+       <Signup />
     </div>
   );
 }
---
+++ my-app/src/container/signup/signup.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import './signup.css'
+
+class Signup extends Component {
+   
+    render(){
+        return <div className="signup-body">
+                    <div className="signup-container">
+                        <div className="signup-head">Signup</div><br/>
+                        <div className="signup-main">
+                                Body
+                        </div>
+                    </div>
+                 </div>
+        }
+
+}
+
+export default Signup
